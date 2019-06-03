30b7f7348fa22c4b040fac877a43548e72be2aec
David Seith <david.seith@cern.ch>
2019-06-03 15:34:59 +0200

    jets_cff: added 2018 jetid
    preliminary version. Still need to add modifiers to automatically choose
    the correct jetid.

--- PhysicsTools/NanoAOD/python/jets_cff.py
+++ PhysicsTools/NanoAOD/python/jets_cff.py
@@ -71,6 +71,21 @@ tightJetIdLepVeto = cms.EDProducer("PatJetIDValueMapProducer",
 			  ),
                           src = cms.InputTag("updatedJets")
 )
+tightJetId18 = cms.EDProducer("PatJetIDValueMapProducer",
+			  filterParams=cms.PSet(
+			    version = cms.string('WINTER18'),
+			    quality = cms.string('TIGHT'),
+			  ),
+                          src = cms.InputTag("updatedJets")
+)
+tightJetIdLepVeto18 = cms.EDProducer("PatJetIDValueMapProducer",
+			  filterParams=cms.PSet(
+			    version = cms.string('WINTER18'),
+			    quality = cms.string('TIGHTLEPVETO'),
+			  ),
+                          src = cms.InputTag("updatedJets")
+)
+
 for modifier in run2_miniAOD_80XLegacy, run2_nanoAOD_94X2016:
     modifier.toModify( tightJetId.filterParams, version = "WINTER16" )
     modifier.toModify( tightJetIdLepVeto.filterParams, version = "WINTER16" )
@@ -143,6 +158,8 @@ updatedJetsWithUserData = cms.EDProducer("PATJetUserDataEmbedder",
      userInts = cms.PSet(
         tightId = cms.InputTag("tightJetId"),
         tightIdLepVeto = cms.InputTag("tightJetIdLepVeto"),
+        tightId18 = cms.InputTag("tightJetId18"),
+        tightIdLepVeto18 = cms.InputTag("tightJetIdLepVeto18"),
         vtxNtrk = cms.InputTag("bJetVars:vtxNtrk"),
         leptonPdgId = cms.InputTag("bJetVars:leptonPdgId"),
         
@@ -214,6 +231,7 @@ jetTable = cms.EDProducer("SimpleCandidateFlatTableProducer",
         #puIdDisc = Var("userFloat('pileupJetId:fullDiscriminant')",float,doc="Pilup ID discriminant",precision=10),
         puId = Var("userInt('pileupJetId:fullId')",int,doc="Pilup ID flags"),
         jetId = Var("userInt('tightId')*2+4*userInt('tightIdLepVeto')",int,doc="Jet ID flags bit1 is loose (always false in 2017 since it does not exist), bit2 is tight, bit3 is tightLepVeto"),
+        jetId18 = Var("userInt('tightId18')*2+4*userInt('tightIdLepVeto18')",int,doc="Jet ID flags bit1 is loose (always false in 2017 since it does not exist), bit2 is tight, bit3 is tightLepVeto"),
         qgl = Var("userFloat('qgl')",float,doc="Quark vs Gluon likelihood discriminator",precision=10),
         nConstituents = Var("numberOfDaughters()",int,doc="Number of particles in the jet"),
         rawFactor = Var("1.-jecFactor('Uncorrected')",float,doc="1 - Factor to get back to raw pT",precision=6),
@@ -558,7 +576,7 @@ from RecoJets.JetProducers.QGTagger_cfi import  QGTagger
 qgtagger=QGTagger.clone(srcJets="updatedJets",srcVertexCollection="offlineSlimmedPrimaryVertices")
 
 #before cross linking
-jetSequence = cms.Sequence(jetCorrFactorsNano+updatedJets+tightJetId+tightJetIdLepVeto+bJetVars+jercVars+qgtagger+updatedJetsWithUserData+jetCorrFactorsAK8+updatedJetsAK8+tightJetIdAK8+tightJetIdLepVetoAK8+updatedJetsAK8WithUserData+chsForSATkJets+softActivityJets+softActivityJets2+softActivityJets5+softActivityJets10+finalJets+finalJetsAK8)
+jetSequence = cms.Sequence(jetCorrFactorsNano+updatedJets+tightJetId+tightJetIdLepVeto+tightJetId18+tightJetIdLepVeto18+bJetVars+jercVars+qgtagger+updatedJetsWithUserData+jetCorrFactorsAK8+updatedJetsAK8+tightJetIdAK8+tightJetIdLepVetoAK8+updatedJetsAK8WithUserData+chsForSATkJets+softActivityJets+softActivityJets2+softActivityJets5+softActivityJets10+finalJets+finalJetsAK8)
 
 _jetSequence_2016 = jetSequence.copy()
 _jetSequence_2016.insert(_jetSequence_2016.index(tightJetId), looseJetId)
