5ef8aad7c7c8bb1990aaf4d4c6c564bf8546e176
David Seith <david.seith@cern.ch>
2020-04-22 17:12:45 +0200

    particle

---
+++ test/GenAnalyzer_cfg.py
@@ -0,0 +1,190 @@
+import FWCore.ParameterSet.Config as cms
+
+# process = cms.Process("GenAnalyzer")
+from Configuration.Eras.Era_Run2_2018_cff import Run2_2018
+process = cms.Process('NANO', Run2_2018)
+
+# process.load('PhysicsTools.NanoAOD.nano_cff')
+# process.load("FWCore.MessageService.MessageLogger_cfi")
+# process.load('Configuration.StandardSequences.EndOfProcess_cff')
+# process.load('Configuration.EventContent.EventContent_cff')
+
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('SimGeneral.MixingModule.mixNoPU_cfi')
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_cff')
+process.load('PhysicsTools.NanoAOD.nano_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+
+process.source = cms.Source("PoolSource",
+                                fileNames = cms.untracked.vstring(
+                               'file.root' 
+                )
+                            )
+process.options = cms.untracked.PSet(
+
+)
+
+from  PhysicsTools.NanoAOD.common_cff import *
+
+process.nanoMetadata = cms.EDProducer("UniqueStringProducer",
+    strings = cms.PSet(
+        tag = cms.string("untagged"),
+    )
+)
+
+process.genWeightsTable = cms.EDProducer('GenWeightsTableProducer',
+        genEvent = cms.InputTag('generator'),
+    lheInfo = cms.VInputTag(cms.InputTag("externalLHEProducer"), cms.InputTag("source")),
+    preferredPDFs = cms.VPSet( # see https://lhapdf.hepforge.org/pdfsets.html
+        cms.PSet( name = cms.string("NNPDF31_nnlo_as_0118_nf_4_mc_hessian"), lhaid = cms.uint32(306046) ),
+        cms.PSet( name = cms.string("NNPDF31_nnlo_hessian_pdfas"), lhaid = cms.uint32(306000) ),
+    ),
+    namedWeightIDs = cms.vstring(),
+    namedWeightLabels = cms.vstring(),
+    lheWeightPrecision = cms.int32(14),
+    maxPdfWeights = cms.uint32(150), 
+    debug = cms.untracked.bool(False),
+)
+
+process.lheInfoTable = cms.EDProducer("LHETablesProducer",
+    lheInfo = cms.VInputTag(cms.InputTag("externalLHEProducer"), cms.InputTag("source")),
+    precision = cms.int32(14),
+    storeLHEParticles = cms.bool(True) 
+)
+
+
+process.finalGenParticles = cms.EDProducer("GenParticlePruner",
+    src = cms.InputTag("genParticles"),
+    select = cms.vstring(
+    "drop *",
+        "keep++ abs(pdgId) == 15 & (pt > 15 ||  isPromptDecayed() )",#  keep full tau decay chain for some taus
+    #"drop status==1 & pt < 1", #drop soft stable particle in tau decay
+        "keep+ abs(pdgId) == 15 ",  #  keep first gen decay product for all tau
+        "+keep pdgId == 22 && status == 1 && (pt > 10 || isPromptFinalState())", # keep gamma above 10 GeV (or all prompt) and its first parent
+    "+keep abs(pdgId) == 11 || abs(pdgId) == 13 || abs(pdgId) == 15", #keep leptons, with at most one mother back in the history
+    "drop abs(pdgId)= 2212 && abs(pz) > 1000", #drop LHC protons accidentally added by previous keeps
+        "keep (400 < abs(pdgId) < 600) || (4000 < abs(pdgId) < 6000)", #keep all B and C hadrons
+        "keep abs(pdgId) == 12 || abs(pdgId) == 14 || abs(pdgId) == 16",   # keep neutrinos
+    "keep status == 3 || (status > 20 && status < 30)", #keep matrix element summary
+        "keep isHardProcess() ||  fromHardProcessDecayed()  || fromHardProcessFinalState() || (statusFlags().fromHardProcess() && statusFlags().isLastCopy())",  #keep event summary based on status flags
+    "keep  (status > 70 && status < 80 && pt > 15) ", # keep high pt partons right before hadronization
+        "keep abs(pdgId) == 23 || abs(pdgId) == 24 || abs(pdgId) == 25 || abs(pdgId) == 37 ",   # keep VIP(articles)s
+        #"keep abs(pdgId) == 310 && abs(eta) < 2.5 && pt > 1 ",                                                     # keep K0
+        "keep (1000001 <= abs(pdgId) <= 1000039 ) || ( 2000001 <= abs(pdgId) <= 2000015)", #keep SUSY fiction particles
+        
+   )
+)
+
+
+
+##################### Tables for final output and docs ##########################
+process.genParticleTable = cms.EDProducer("SimpleCandidateFlatTableProducer",
+    src = cms.InputTag("finalGenParticles"),
+    cut = cms.string(""), #we should not filter after pruning
+    name= cms.string("GenPart"),
+    doc = cms.string("interesting gen particles "),
+    singleton = cms.bool(False), # the number of entries is variable
+    extension = cms.bool(False), # this is the main table for the taus
+    variables = cms.PSet(
+         pt  = Var("pt",  float, precision=8),
+         phi = Var("phi", float,precision=8),
+         eta  = Var("eta",  float,precision=8),
+         mass = Var("?mass>10 || (pdgId==22 && mass > 1) || abs(pdgId)==24 || pdgId==23 || abs(pdgId)>1000000?mass:0", float,precision="?(abs(pdgId)==6 && statusFlags().isLastCopy())?20:8",doc="Mass stored for all particles with mass > 10 GeV and photons with mass > 1 GeV, plus W/Z and BSM particles. For other particles you can lookup from PDGID"),
+         pdgId  = Var("pdgId", int, doc="PDG id"),
+         status  = Var("status", int, doc="Particle status. 1=stable"),
+         genPartIdxMother = Var("?numberOfMothers>0?motherRef(0).key():-1", int, doc="index of the mother particle"),
+         statusFlags = (Var(
+            "statusFlags().isLastCopyBeforeFSR()                  * 16384 +"
+            "statusFlags().isLastCopy()                           * 8192  +"
+            "statusFlags().isFirstCopy()                          * 4096  +"
+            "statusFlags().fromHardProcessBeforeFSR()             * 2048  +"
+            "statusFlags().isDirectHardProcessTauDecayProduct()   * 1024  +"
+            "statusFlags().isHardProcessTauDecayProduct()         * 512   +"
+            "statusFlags().fromHardProcess()                      * 256   +"
+            "statusFlags().isHardProcess()                        * 128   +"
+            "statusFlags().isDirectHadronDecayProduct()           * 64    +"
+            "statusFlags().isDirectPromptTauDecayProduct()        * 32    +"
+            "statusFlags().isDirectTauDecayProduct()              * 16    +"
+            "statusFlags().isPromptTauDecayProduct()              * 8     +"
+            "statusFlags().isTauDecayProduct()                    * 4     +"
+            "statusFlags().isDecayedLeptonHadron()                * 2     +"
+            "statusFlags().isPrompt()                             * 1      ",
+            int, doc=("gen status flags stored bitwise, bits are: "
+                "0 : isPrompt, "
+                "1 : isDecayedLeptonHadron, "
+                "2 : isTauDecayProduct, "
+                "3 : isPromptTauDecayProduct, "
+                "4 : isDirectTauDecayProduct, "
+                "5 : isDirectPromptTauDecayProduct, "
+                "6 : isDirectHadronDecayProduct, "
+                "7 : isHardProcess, "
+                "8 : fromHardProcess, "
+                "9 : isHardProcessTauDecayProduct, "
+                "10 : isDirectHardProcessTauDecayProduct, "
+                "11 : fromHardProcessBeforeFSR, "
+                "12 : isFirstCopy, "
+                "13 : isLastCopy, "
+                "14 : isLastCopyBeforeFSR, ")
+            )),
+    )
+)
+
+
+process.configurationMetadata = cms.untracked.PSet(
+    annotation = cms.untracked.string('step1 nevts:100'),
+    name = cms.untracked.string('Applications'),
+    version = cms.untracked.string('$Revision: 1.19 $')
+)
+
+process.NANOEDMAODSIMoutput = cms.OutputModule("NanoAODOutputModule",
+    compressionAlgorithm = cms.untracked.string('LZMA'),
+    compressionLevel = cms.untracked.int32(9),
+    dataset = cms.untracked.PSet(
+        dataTier = cms.untracked.string('NANOAODSIM'),
+        filterName = cms.untracked.string('')
+    ),
+    fileName = cms.untracked.string('TOP_nanoaod.root'),
+    outputCommands = process.NANOAODSIMEventContent.outputCommands
+)
+
+
+print process.NANOAODSIMEventContent.outputCommands
+
+process.nanoSequence = cms.Sequence(process.nanoMetadata * process.genWeightsTable * process.lheInfoTable * process.finalGenParticles * process.genParticleTable)
+
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, '106X_upgrade2018_realistic_v4', '')
+
+
+process.nanoAOD_step = cms.Path(process.nanoSequence)
+process.endjob_step = cms.EndPath(process.endOfProcess)
+process.NANOEDMAODSIMoutput_step = cms.EndPath(process.NANOEDMAODSIMoutput)
+process.schedule = cms.Schedule(process.nanoAOD_step, process.endjob_step, process.NANOEDMAODSIMoutput_step)
+
+from PhysicsTools.PatAlgos.tools.helpers import associatePatAlgosToolsTask
+associatePatAlgosToolsTask(process)
+
+# process.options.numberOfThreads=cms.untracked.uint32(48)
+# process.options.numberOfStreams=cms.untracked.uint32(0)
+# process.options.numberOfConcurrentLuminosityBlocks=cms.untracked.uint32(1)
+
+
+
+# Automatic addition of the customisation function from Configuration.DataProcessing.Utils
+from Configuration.DataProcessing.Utils import addMonitoring 
+
+#call to customisation function addMonitoring imported from Configuration.DataProcessing.Utils
+process = addMonitoring(process)
+
+# Add early deletion of temporary data products to reduce peak memory need
+from Configuration.StandardSequences.earlyDeleteSettings_cff import customiseEarlyDelete
+process = customiseEarlyDelete(process)
+# End adding early deletion
---
+++ test/TOP_gen_halfchain_cfg.py
@@ -0,0 +1,174 @@
+# Auto generated configuration file
+# using: 
+# Revision: 1.19 
+# Source: /local/reps/CMSSW/CMSSW/Configuration/Applications/python/ConfigBuilder.py,v 
+# with command line options: Configuration/GenProduction/python/TOP-RunIISummer19UL17wmLHEGEN-00055-fragment.py --fileout file:TOP_gen_antitop.root --mc --eventcontent RAWSIM,LHE --datatier GEN,LHE --conditions 106X_upgrade2018_realistic_v4 --beamspot Realistic25ns13TeVEarly2018Collision --step LHE,GEN --geometry DB:Extended --era Run2_2018 --python_filename TOP_gen_cfg.py --no_exec --customise Configuration/DataProcessing/Utils.addMonitoring --customise_commands process.RandomNumberGeneratorService.externalLHEProducer.initialSeed=int(5552) -n 10
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.Eras.Era_Run2_2018_cff import Run2_2018
+
+
+import FWCore.ParameterSet.VarParsing as VarParsing
+options = VarParsing.VarParsing()
+
+options.register('tarBall', '', VarParsing.VarParsing.multiplicity.singleton,VarParsing.VarParsing.varType.string,"name of tar ball")
+
+options.parseArguments()
+
+process = cms.Process('GEN',Run2_2018)
+
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('SimGeneral.MixingModule.mixNoPU_cfi')
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_cff')
+process.load('Configuration.StandardSequences.Generator_cff')
+process.load('IOMC.EventVertexGenerators.VtxSmearedRealistic25ns13TeVEarly2018Collision_cfi')
+process.load('GeneratorInterface.Core.genFilterSummary_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+process.maxEvents = cms.untracked.PSet(
+    input=cms.untracked.int32(5000)
+)
+
+# Input source
+process.source = cms.Source("EmptySource")
+
+process.options = cms.untracked.PSet(
+
+)
+
+# Production Info
+process.configurationMetadata = cms.untracked.PSet(
+    annotation = cms.untracked.string('Configuration/GenProduction/python/TOP-RunIISummer19UL17wmLHEGEN-00055-fragment.py nevts'),
+    name = cms.untracked.string('Applications'),
+    version = cms.untracked.string('$Revision: 1.19 $')
+)
+
+# Output definition
+
+process.RAWSIMoutput = cms.OutputModule("PoolOutputModule",
+    SelectEvents = cms.untracked.PSet(
+        SelectEvents = cms.vstring('generation_step')
+    ),
+    compressionAlgorithm = cms.untracked.string('LZMA'),
+    compressionLevel = cms.untracked.int32(1),
+    dataset = cms.untracked.PSet(
+        dataTier = cms.untracked.string('GEN'),
+        filterName = cms.untracked.string('')
+    ),
+    eventAutoFlushCompressedSize = cms.untracked.int32(20971520),
+    fileName = cms.untracked.string('TOP_nano.root'),
+    outputCommands = process.RAWSIMEventContent.outputCommands,
+    splitLevel = cms.untracked.int32(0)
+)
+
+process.LHEoutput = cms.OutputModule("PoolOutputModule",
+    dataset = cms.untracked.PSet(
+        dataTier = cms.untracked.string('LHE'),
+        filterName = cms.untracked.string('')
+    ),
+    fileName = cms.untracked.string('TOP_inLHE.root'),
+    outputCommands = process.LHEEventContent.outputCommands,
+    splitLevel = cms.untracked.int32(0)
+)
+
+# Additional output definition
+
+# Other statements
+process.genstepfilter.triggerConditions=cms.vstring("generation_step")
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, '106X_upgrade2018_realistic_v4', '')
+
+# import FWCore.ParameterSet.Config as cms
+# from Configuration.Generator.Pythia8CommonSettings_cfi import *
+# from Configuration.Generator.MCTunes2017.PythiaCP5Settings_cfi import *
+# from Configuration.Generator.Pythia8aMCatNLOSettings_cfi import *
+# from Configuration.Generator.PSweightsPythia.PythiaPSweightsSettings_cfi import *
+
+from Configuration.Generator.Pythia8CommonSettings_cfi import *
+from Configuration.Generator.MCTunes2017.PythiaCP5Settings_cfi import *
+from Configuration.Generator.PSweightsPythia.PythiaPSweightsSettings_cfi import *
+
+process.generator = cms.EDFilter("Pythia8HadronizerFilter",
+    maxEventsToPrint = cms.untracked.int32(1),
+    pythiaPylistVerbosity = cms.untracked.int32(1),
+    filterEfficiency = cms.untracked.double(1.0),
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    comEnergy = cms.double(13000.),
+    PythiaParameters = cms.PSet(
+        pythia8CommonSettingsBlock,
+        pythia8CP5SettingsBlock,
+        pythia8PSweightsSettingsBlock,
+        parameterSets = cms.vstring('pythia8CommonSettings',
+                                    'pythia8CP5Settings',
+                                    'pythia8PSweightsSettings',
+                                    )
+    )
+)
+
+
+
+process.ProductionFilterSequence = cms.Sequence(process.generator)
+
+
+process.externalLHEProducer = cms.EDProducer("ExternalLHEProducer",
+    # args = cms.vstring('root://cms-xrd-global.cern.ch//store/user/dseith/gridpacks/st_tch_top_slc7_amd64_gcc700_CMSSW_10_2_17_tarball.tar.xz'),
+    # args = cms.vstring('/store/user/dseith/gridpacks/st_tch_top_slc7_amd64_gcc700_CMSSW_10_2_17_tarball.tar.xz'),
+    # args = cms.vstring('srm://cmssrm-kit.gridka.de:8443/srm/managerv2?SFN=/pnfs/gridka.de/cms/disk-only/store/user/dseith/gridpacks/st_tch_top_slc7_amd64_gcc700_CMSSW_10_2_17_tarball.tar.xz'),
+    ## args = cms.vstring('/pnfs/gridka.de/cms/disk-only/store/user/dseith/gridpacks/st_tch_top_slc7_amd64_gcc700_CMSSW_10_2_17_tarball.tar.xz'),
+    # args = cms.vstring('../st_tch_top_fullchain_slc7_amd64_gcc700_CMSSW_10_2_17_tarball.tar.xz'), # ../st_tch_top_madgraph_slc7_amd64_gcc700_CMSSW_10_2_17_tarball.tar.xz'), #'../st_tch_top_slc7_amd64_gcc700_CMSSW_10_2_17_tarball.tar.xz'),
+    # args = cms.vstring('st_tch_top_halfchain_ctW_0p02_ctWI_m3p67_cpQ3_4p1_cQq13_3p51_cQq83_m7p21_slc7_amd64_gcc700_CMSSW_10_2_17_tarball.tar.xz'), # ../st_tch_top_madgraph_slc7_amd64_gcc700_CMSSW_10_2_17_tarball.tar.xz'), #'../st_tch_top_slc7_amd64_gcc700_CMSSW_10_2_17_tarball.tar.xz'),
+    args = cms.vstring(options.tarBall),
+    nEvents = cms.untracked.uint32(5000),
+    numberOfParameters = cms.uint32(1),
+    outputFile = cms.string('cmsgrid_final.lhe'),
+    scriptName = cms.FileInPath('GeneratorInterface/LHEInterface/data/run_generic_tarball_cvmfs.sh')
+)
+
+
+# process.ProductionFilterSequence = cms.Sequence(process.generator)
+
+# Path and EndPath definitions
+process.lhe_step = cms.Path(process.externalLHEProducer)
+process.generation_step = cms.Path(process.pgen)
+process.genfiltersummary_step = cms.EndPath(process.genFilterSummary)
+process.endjob_step = cms.EndPath(process.endOfProcess)
+process.RAWSIMoutput_step = cms.EndPath(process.RAWSIMoutput)
+process.LHEoutput_step = cms.EndPath(process.LHEoutput)
+
+# Schedule definition
+process.schedule = cms.Schedule(process.lhe_step,process.generation_step,process.genfiltersummary_step,process.endjob_step,process.RAWSIMoutput_step,process.LHEoutput_step)
+from PhysicsTools.PatAlgos.tools.helpers import associatePatAlgosToolsTask
+associatePatAlgosToolsTask(process)
+# filter all path with the production filter sequence
+for path in process.paths:
+	if path in ['lhe_step']: continue
+	getattr(process,path).insert(0, process.ProductionFilterSequence)
+
+# customisation of the process.
+# process.options.numberOfThreads=cms.untracked.uint32(20)
+process.options.numberOfStreams=cms.untracked.uint32(0)
+process.options.numberOfConcurrentLuminosityBlocks=cms.untracked.uint32(1)
+
+
+
+# Automatic addition of the customisation function from Configuration.DataProcessing.Utils
+from Configuration.DataProcessing.Utils import addMonitoring 
+
+#call to customisation function addMonitoring imported from Configuration.DataProcessing.Utils
+process = addMonitoring(process)
+
+# End of customisation functions
+
+# Customisation from command line
+
+process.RandomNumberGeneratorService.externalLHEProducer.initialSeed=int(5552)
+# Add early deletion of temporary data products to reduce peak memory need
+from Configuration.StandardSequences.earlyDeleteSettings_cff import customiseEarlyDelete
+process = customiseEarlyDelete(process)
+# End adding early deletion
